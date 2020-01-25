8b271386f7477abbac32f79fb3f399f33ac3ea8a
re-andrade1987 <renataandrade1987@hotmail.com>
2020-01-25 18:32:28 -0300

    Commit inicial teste

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-function App() {
+
+const Users = () => {
+  const [usersList, setUsersList] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const getUsers = await api.get('users');
+      setUsersList(getUsers.data)
+
+    }
+    loadData()
+
+  }, []);
+  console.log(usersList)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de contatos</h1>
+      <div className='form'>
+        <form >
+          <input type='text' placeholder='Pesquisar...' />
+          <button>Enviar</button>
+        </form>
+      </div>
+      <div className='main-info-cards'>
+        {usersList.map((users, name) =>
+          <div className='info-cards' key={name.name}>
+            <h3>Dados do cliente</h3>
+            <p >Name: {users.name}</p>
+            <p>Username: {users.username}</p>
+            <p>Email: {users.email}</p>
+            <h3>Endereço</h3>
+            <p>street: {users.address.street}</p>
+            <p>Suite: {users.address.suite}</p>
+            <p>City: {users.address.city}</p>
+            <p>Zipcode: {users.address.zipcode}</p>
+            <h3>Contatos</h3>
+            <p>Phone: {users.phone}</p>
+            <p>website: {users.website}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Users;
