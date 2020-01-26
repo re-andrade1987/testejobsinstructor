96bc97ba4c3bd476465e7191c6355b8a9e399949
re-andrade1987 <renataandrade1987@hotmail.com>
2020-01-26 01:42:28 -0300

    Trazendo dados no search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import api from './services/api';
 
+const people = [
+  ".biz",
+  "org",
+  ".net",
+  ".com",
+  ".ca",
+  ".tv",
+  ".info",
+  "me",
+  "io"
+];
+
 
 const Users = () => {
   const [usersList, setUsersList] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value)
+  }
 
   useEffect(() => {
     const loadData = async () => {
-      const getUsers = await api.get('users');
+      const getUsers = await api.get('users')
       setUsersList(getUsers.data)
 
     }
@@ -16,12 +33,23 @@ const Users = () => {
   }, []);
   console.log(usersList)
 
+ /* useEffect(() => {
+    const results = people.filter(person =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    usersList(results);
+  }, [searchTerm]);*/
+
   return (
     <div className="App">
-      <h1>Lista de contatos</h1>
+      <h1>Ficha de dados dos clientes</h1>
       <div className='form'>
         <form >
-          <input type='text' placeholder='Pesquisar...' />
+          <input type='text'
+            placeholder='Pesquisar...'
+            value={searchTerm}
+            onChange={handleChange}
+          />
           <button>Enviar</button>
         </form>
       </div>
@@ -29,17 +57,17 @@ const Users = () => {
         {usersList.map((users, name) =>
           <div className='info-cards' key={name.name}>
             <h3>Dados do cliente</h3>
-            <p >Name: {users.name}</p>
-            <p>Username: {users.username}</p>
+            <p >Nome: {users.name}</p>
+            <p>Sobrenome: {users.username}</p>
             <p>Email: {users.email}</p>
             <h3>Endereço</h3>
-            <p>street: {users.address.street}</p>
-            <p>Suite: {users.address.suite}</p>
-            <p>City: {users.address.city}</p>
-            <p>Zipcode: {users.address.zipcode}</p>
+            <p>Rua: {users.address.street}</p>
+            <p>Número: {users.address.suite}</p>
+            <p>Cidade: {users.address.city}</p>
+            <p>Cep: {users.address.zipcode}</p>
             <h3>Contatos</h3>
-            <p>Phone: {users.phone}</p>
-            <p>website: {users.website}</p>
+            <p>Telefone: {users.phone}</p>
+            <p>Website: {users.website}</p>
           </div>
         )}
       </div>
